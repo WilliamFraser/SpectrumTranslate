abe90a58bec4f161077166c2176d82e4142efe5b
William Fraser <william.fraser@virgin.net>
2015-01-17 20:27:55 +0000

    Added licence.

--- spectrumnumber.py
+++ spectrumnumber.py
@@ -1,3 +1,35 @@
+# This file is part of the SpectrumTranslate python module.
+#
+# It's licenced under GPL version 3 (www.gnu.org/licenses/gpl.html) with a few extra stipulations:
+# 1) These first lines in this file as far as the line with the date needs to be left in so anyone
+# who gets a copy of this file has access to the licence, extra stipulations, and disclaimors.
+# 2) If this code is used as part of another project, I'd apreciate a mention in that project's
+# documentation.
+# 3) If you improve on any of the routines, I'd be most grateful if you would pass them back to
+# me so that I can have the option to incorporate them into the origional module with apropriate
+# attribution under this licence and stipulations.
+#
+# A copy of the licence and stipulations is bundled with the source files as licence.txt, or you
+# can go to the GNU website for the terms of the GPL licence.
+#
+# If you try hard enough, I'm sure someone could damage something (software, data, system, hardware)
+# useing it. I've put a lot of time and effort into this software, and have removed any obvious
+# bugs, but nothing is perfect. If you spot any flaws, please let me know so that I might be able to
+# fix them. However I reserve the right not to fix flaws that I don't have the time, or resources
+# to fix, or that I feel that fixing would detriment the software overall. By useing this software
+# you accept this, and any potential risk to your own hardware, software, data, and/or physical and
+# mental health. This software is provided "as is" and any express or implied warranties,
+# including, but not limited to, the implied warranties of merchantability and fitness for a
+# particular purpose are disclaimed. In no event shall I or any contributors be liable for any
+# direct, indirect, incidental, special, exemplary, or consequential damages (including, but not
+# limited to, procurement of substitute goods or services; loss of use, data, or profits; or
+# business interruption) however caused and on any theory of liability, whether in contract, strict
+# liability, or tort (including negligence or otherwise) arising in any way out of the use of this
+# software, even if advised of the possibility of such damage.
+#
+# Author: william.fraser@virgin.net
+# Date: 14th January 2015
+
 class SpectrumNumberException(Exception):
     """
     A class to flag up an Exception raised during working with SpectrumNumber class objects
